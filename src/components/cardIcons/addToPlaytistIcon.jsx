de534c03120d65894c49b02a5163917962638b09
Darragh Conneely <darraghc23@gmail.com>
2023-06-19 18:52:19 +0100

    New button for Upcoming movies page

---
+++ src/components/cardIcons/addToPlaytistIcon.jsx
@@ -0,0 +1,20 @@
+//import React, { useContext } from "react";
+//import { PlaylistContext } from "../../contexts/moviesContext";
+import IconButton from "@mui/material/IconButton";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+
+const AddToPlaytistIcon = ({ movie }) => {
+  //const context = useContext(PlaylistContext);
+
+  const onUserSelect = (e) => {
+    e.preventDefault();
+    context.addToPlaylist(movie);
+  };
+  return (
+    <IconButton aria-label="add to playlist" onClick={onUserSelect}>
+      <PlaylistAddIcon color="primary" fontSize="large" />
+    </IconButton>
+  );
+};
+
+export default AddToPlaytistIcon;
--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -3,7 +3,7 @@ import PageTemplate from '../components/templateMovieListPage';
 import { getUpcomingMovies } from "../api/tmdb-api";
 import { useQuery } from "react-query";
 import Spinner from "../components/spinner";
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites'
+import AddToPlaytistIcon from '../components/cardIcons/addToPlaytistIcon'
 
 const UpcomingMoviesPage = (props) => {
   const { data, error, isLoading, isError } = useQuery("upcoming", getUpcomingMovies);
@@ -27,7 +27,7 @@ const UpcomingMoviesPage = (props) => {
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
-        return <AddToFavouritesIcon movie={movie} />
+        return <AddToPlaytistIcon movie={movie} />
       }}
     />
   );
